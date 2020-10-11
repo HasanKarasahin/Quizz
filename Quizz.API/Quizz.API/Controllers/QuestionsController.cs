d8952c393e4337589e11c915fa6bcc6877dbf32d
Hasan Gazi <hasn_07@hotmail.com>
2020-10-12 02:08:16 +0300

    Api de Controller oluşturuldu.

---
+++ Quizz.API/Quizz.API/Controllers/QuestionsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizz.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Merhaba Dünya";
+        }
+    }
+}
